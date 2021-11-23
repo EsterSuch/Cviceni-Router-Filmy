42604bb2f8be9a520d0d17f970e5c0da37c49417
Ester Suchanová <p.res@centrum.cz>
2021-11-23 21:06:49 +0100

    pokus

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import movies from './../../movies.js';
+import { useParams } from 'react-router-dom';
 
 const Movie = () => {
-	// naimportuj useParams z react-router-dom
-	// pomocí useParams získej id filmu předané jako parametr v adrese, která zobrazila tuto komponentu
-	// podle id najdi film v poli movies (už ho máš naimportované)
-	// místo komentáře dole zobraz detaily nalezeného filmu
+  // naimportuj useParams z react-router-dom
+  // pomocí useParams získej id filmu předané jako parametr v adrese, která zobrazila tuto komponentu
+  // podle id najdi film v poli movies (už ho máš naimportované)
+  // místo komentáře dole zobraz detaily nalezeného filmu
 
-	return (
-		<div className="movie">
-			{/*
+  const { id } = useParams();
+
+  return (
+    <div className="movie">
+      {/*
 				Sem doplnit:
 				- obrázek (jsou ve složce assets)
 				- název filmu
 				- popis
 				- případně další detaily
 			*/}
-		</div>
-	)
+    </div>
+  )
 }
 
 export default Movie;
--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import movies from './../../movies.js';
+import { Link } from 'react-router-dom';
 
 const MovieList = () => {
-	return (
-		<nav className="movie-list">
-			{
-				movies.map((movie) => (
-					<p key={movie.id}>{movie.title}</p>
-				))
-			}
+  return (
+    <nav className="movie-list">
+      <ul>
+        {
+          movies.map(movie => (
+            //<p key={movie.id}>{movie.title}</p>
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
+          ))
+
+        }
+      </ul>
     </nav>
-	)
+
+
+
+  )
+
 }
 
 export default MovieList;
